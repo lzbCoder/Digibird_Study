128332cd15dfad5e9e5b16e4dd43de3b1baf65ac
lizhibin <${1838558217@qq.com}>
2021-05-09 17:21:18 +0800

    测试方法修改

--- java-model-demo/src/main/java/com/lzb/model/factorymethod/TestMain.java
+++ java-model-demo/src/main/java/com/lzb/model/factorymethod/TestMain.java
@@ -6,7 +6,7 @@ public class TestMain {
         //创建一个工厂对象
         BarbecueFactory factory = new BarbecueFactoryA();
         //通过工厂来获取一个烧烤店
-        BarbecueShop shop = factory.getBarbecue();
+        BarbecueShop shop = factory.getBarbecueShop();
         //调用方法
         shop.saleKebab();
     }
